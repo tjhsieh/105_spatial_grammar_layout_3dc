622514b15a05c7375162aca53e324c0bda8b4450
tjhsieh <42711557+tjhsieh@users.noreply.github.com>
2025-12-02 10:39:00 +0800

    Add files via upload

---
+++ RulesIntegrate/scripts/Rules.js
@@ -0,0 +1,247 @@
+﻿var Rules = [
+	   {
+		   no: 1,
+		   description1: '依據中軸線向左右衍生',
+		   description2: '初始間生成'
+	   }, 
+	   {
+		   no: 2,
+		   description1: '依垂直方向向上方衍生',
+		   description2: '文獻規則2'
+	   },
+	   {
+		   no: 3,
+		   description1: '依據中軸線隔前一次衍生的間向左右衍生',
+		   description2: '文獻規則1與3合併'
+	   },
+	   {
+		   no: 4,
+		   description1: '缺角圖形衍生至方正',
+		   description2: '文獻規則4'
+	   },
+	   {
+		   no: 5,
+		   description1: '缺角圖形衍生至方正',
+		   description2: '文獻規則5'
+	   },
+	   {
+		   no: 6,
+		   description1: '副階生成方式',
+		   description2: '文獻規則6與7合併'
+	   },
+	   {
+		   no: 7,
+		   description1: '副階生成方式',
+		   description2: '文獻規則8'
+	   },
+	   {
+		   no: 8,
+		   description1: '副階生成方式',
+		   description2: '文獻規則9'
+	   },
+	   {
+		   no: 9,
+		   description1: '分槽原則',
+		   description2: '文獻規則10'
+	   },
+	   {
+		   no: 10,
+		   description1: '分槽原則',
+		   description2: '文獻規則11'
+	   },
+	   {
+		   no: 11,
+		   description1: '分槽原則',
+		   description2: '文獻規則12'
+	   },
+	   {
+		   no: 12,
+		   description1: '分槽原則',
+		   description2: '文獻規則13'
+	   },
+	   {
+		   no: 13,
+		   description1: '分槽原則',
+		   description2: '文獻規則16與19合併'
+	   },
+	   {
+		   no: 14,
+		   description1: '分槽原則',
+		   description2: '文獻規則15與18合併'
+	   },
+	   {
+		   no: 15,
+		   description1: '當心間補間鋪作數二朵',
+		   description2: '文獻規則18'
+	   },
+	   {
+		   no: 16,
+		   description1: '定義柱頭鋪作',
+		   description2: '文獻規則15'
+	   },
+	   {
+		   no: 17,
+		   description1: '定義補間鋪作位置',
+		   description2: '文獻規則17、20與21合併'
+	   },
+	   {
+		   no: 18,
+		   description1: '定義梢間補間鋪作位置',
+		   description2: '文獻規則22至23合併'
+	   },
+	   {
+		   no: 19,
+		   description1: '定義柱頭鋪作即是落柱位置',
+		   description2: '文獻規則24至25合併'
+	   },
+	   {
+		   no: 20,
+		   description1: '定義鋪間鋪作位置',
+		   description2: '文獻規則26'
+	   },
+	   {
+		   no: 21,
+		   description1: '定義鋪間鋪作位置',
+		   description2: '文獻規則27'
+	   },
+	   {
+		   no: 22,
+		   description1: '定義一般牆身',
+		   description2: '文獻29、31與32合併'
+	   },
+	   {
+		   no: 23,
+		   description1: '定義開口在a側的牆身',
+		   description2: '文獻規則38'
+	   },
+	   {
+		   no: 24,
+		   description1: '定義開口在a側的牆身',
+		   description2: '文獻規則38'
+	   },
+	   {
+		   no: 25,
+		   description1: '定義開口在a側的牆身',
+		   description2: '文獻規則30、33與36合併'
+	   },
+	   {
+		   no: 26,
+		   description1: '定義縱向的牆身',
+		   description2: '文獻29、31與32合併'
+	   },
+	   {
+		   no: 27,
+		   description1: '定義縱向的牆身',
+		   description2: '文獻規則30、33與36合併'
+	   },
+	   {
+		   no: 28,
+		   description1: '定義方向與wd平行的牆身',
+		   description2: '文獻規則44'
+	   },
+	   {
+		   no: 29,
+		   description1: '定義開口在a右側的牆身為門',
+		   description2: '文獻規則45'
+	   },
+	   {
+		   no: 30,
+		   description1: '定義開口在a側的牆身為窗',
+		   description2: '定義屋頂生成'
+	   },
+	   {
+		   no: 31,
+		   description1: '定義當心間門',
+		   description2: ''
+	   },
+	   {
+		   no: 32,
+		   description1: '定義一般間門在a另一側',
+		   description2: ''
+	   },
+	   {
+		   no: 33,
+		   description1: '定義開口在a側的牆身為窗',
+		   description2: ''
+	   },
+	   {
+		   no: 34,
+		   description1: '定義一般牆身為門',
+		   description2: ''
+	   },
+	   {
+		   no: 35,
+		   description1: '定義一般牆身為窗',
+		   description2: ''
+	   },
+	   {
+		   no: 36,
+		   description1: '定義門兩側為窗',
+		   description2: ''
+	   },
+	   {
+		   no: 37,
+		   description1: '定義當心間兩側為門',
+		   description2: ''
+	   },
+	   {
+		   no: 38,
+		   description1: '定義縱向牆兩側為窗',
+		   description2: ''
+	   },
+	   {
+		   no: 39,
+		   description1: '定義中央門為可開啟的門',
+		   description2: ''
+	   },
+	   {
+		   no: 40,
+		   description1: '定義窗戶為可開啟的形態',
+		   description2: ''
+	   },
+	   {
+		   no: 41,
+		   description1: '定義門左為柱子',
+		   description2: ''
+	   },
+	   {
+		   no: 42,
+		   description1: '定義門右為柱子',
+		   description2: ''
+	   },
+	   {
+		   no: 43,
+		   description1: '定義一般門為可開啟狀態',
+		   description2: ''
+	   },
+	   {
+		   no: 44,
+		   description1: '前方階梯定義',
+		   description2: ''
+	   },
+	   {
+		   no: 45,
+		   description1: '後方階梯定義',
+		   description2: ''
+	   },
+];
+
+
+
+$(document).ready(function ()
+{
+    var str = '';
+    $.each(Rules, function (key, row)
+    {
+		//Rule level
+		str += '<tr>'
+			+ '<td>' + row.no + '</td>'
+			+ '<td>' + row.description1 + '</td>'
+			+ '<td>' + row.description2 + '</td>'
+			+ '</tr>'
+    });
+
+    $('#ruleTable tbody').append(
+            str
+        );
+});
